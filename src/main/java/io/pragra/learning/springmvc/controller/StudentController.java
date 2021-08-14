840b2692d6853ddbf6fa72e13cfe8e8105168580
Atin Singh <atin@pragra.io>
2021-08-14 14:47:29 -0400

    updated with post

--- src/main/java/io/pragra/learning/springmvc/controller/StudentController.java
+++ src/main/java/io/pragra/learning/springmvc/controller/StudentController.java
@@ -1,10 +1,13 @@
 package io.pragra.learning.springmvc.controller;
 
+import io.pragra.learning.springmvc.entity.Student;
 import io.pragra.learning.springmvc.service.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -16,5 +19,19 @@ public class StudentController {
     public String getAllStudent(Model model){
         model.addAttribute("students", service.getAll());
         return "student";
+    }
+
+    @PostMapping("/student")
+    public String createStudent(@ModelAttribute("student") Student student, Model model){
+        service.createStudent(student);
+        model.addAttribute("students", service.getAll());
+        return "student";
+    }
+
+
+    @GetMapping("/addstudent")
+    public String getAllStudentS(Model model){
+        model.addAttribute("student", new Student());
+        return "addstudent";
     }
 }
